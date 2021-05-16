537c8bcd05b31be3b42da2b7e1181b577f1e6304
Ryan Malesic <ryanmalesic@icloud.com>
2021-05-16 16:19:44 -0400

    Fix deployment settings

--- packages/client/amplify.ts
+++ packages/client/amplify.ts
@@ -1,11 +1,11 @@
 export function getClientId(): string {
   return process.env.NODE_ENV === "production"
-    ? "30r12k765sf0hmn6lt8g3srv6u"
+    ? "7reeltkn62gg4kt3elv5gapuof"
     : "30r12k765sf0hmn6lt8g3srv6u";
 }
 
 export function getUserPoolId(): string {
   return process.env.NODE_ENV === "production"
-    ? "us-east-1_z2oQ0gpqP"
+    ? "us-east-1_IIegZQY4q"
     : "us-east-1_z2oQ0gpqP";
 }
